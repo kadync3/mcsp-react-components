6f01e2403e2e3c39b6c88addea450614dee5ae63
kadync3 <106933582+kadync3@users.noreply.github.com>
2022-11-28 13:18:55 -0500

    got inital components to render to the dom

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // TODO
+// import React from 'react';
+// import  ReactDOM from 'react-dom/client';
+
+var GroceryList = ()=>(
+  <ul>
+    <li>Milk</li>
+    <li>Eggs</li>
+  </ul>
+)
+
+ReactDOM.render(<GroceryList/>, document.getElementById("app") )
