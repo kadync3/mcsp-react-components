cea49513698f28f0d2da787f405a8e6e609178f9
kadync3 <106933582+kadync3@users.noreply.github.com>
2022-11-28 14:42:14 -0500

    created functional component reusable list with click event

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 // TODO
 // import React from 'react';
 // import  ReactDOM from 'react-dom/client';
-
+var Items = (props)=>{
+  var onListItemClick = (e)=>{
+    console.log('I have been clicked')
+  }
+  return(
+<ul onClick={onListItemClick}>
+  <li >{props.groceries[0]}</li>
+  <li>{props.groceries[1]}</li>
+</ul>
+  )
+  };
 var GroceryList = ()=>(
-  <ul>
-    <li>Milk</li>
-    <li>Eggs</li>
+  
+  <ul> Grocery List
+    <Items groceries={['Milkarue', 'Egga']} />
+    
   </ul>
 )
 
